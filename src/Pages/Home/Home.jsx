bf6d5872d736d2b388f2946b804ef6083bbe71dd
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-10-20 17:35:11 +0500

    Create Home page.

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import { Hero } from '../../Components/HomeHero/Hero';
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+    </>
+  )
+}
+
+export default Home
