a172bea761eac9fa3a463efdcce37252bdc88fea
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-10-21 00:18:34 +0500

    Completed 'about page'

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import AboutHero from '../../Components/AboutHero/AboutHero';
-import './AboutUs.css';
 import AbWCU from '../../Components/AbtWCU/AbWCU';
+import joinBg from '../../Assets/join-bg.jpg';
+import './AboutUs.css';
 
 const AboutUs = () => {
   return (
@@ -20,6 +21,21 @@ const AboutUs = () => {
         </div>
 
         <AbWCU />
+
+        <div className="aboutSec">
+            <h3>Our Mission</h3>
+            <p className="aboutSecText">Our mission at Fully Constructed is to empower New York residents and businesses with the right construction and renovation solutions, fostering security and peace of mind. We strive to make the construction and renovation process seamless, stress-free, and rewarding, ensuring that you receive the results you deserve without any compromise.</p>
+        </div>
+
+        <div className="aboutSec">
+            <h3>Community Involvement</h3>
+            <p className="aboutSecText">We believe in giving back to the community that has embraced us so warmly. Through various initiatives, sponsorships, and volunteer work, we actively contribute to the betterment of New York and its residents.</p>
+        </div>
+
+        <div className="joinUs">
+            <img src={joinBg} alt="Join Us" />
+            <p>Join us at Fully Constructed, and experience the difference a dedicated construction and renovation partner can make. Together, we'll bring your construction and renovation dreams to life, safeguarding your vision and aspirations with top-notch services. Expect Excellence, and rest assured, we've got your projects covered.</p>
+        </div>
     </>
   )
 }
