2a015d0556b0c98ff31933762ffccfaf82b57514
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-10-20 17:34:53 +0500

    Create hero section fo rthe home page.

---
+++ src/Components/HomeHero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import bg from '../../Assets/main-bg.jpg';
+import './Hero.css';
+import { Link } from 'react-router-dom';
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+      <img src={bg} alt="" className="heroImg" />
+        <div className="heroDesc">
+          <h1 className="heroTitle">Fully Constructed: Gateway to Building Dreams</h1>
+          <h2 className="heroSubTitle">Empowering New York with Quality Construction and Renovation Solutions</h2>
+          <Link to="/get_a_quote"><button className='quote-btn heroBtn'>Get a Quote</button></ Link>
+        </div>
+    </div>
+  )
+}
