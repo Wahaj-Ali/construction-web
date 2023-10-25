8d727eb972cd5dee2239db07972c089524d24fd3
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-10-26 03:28:59 +0500

    Create 'Services' section for the home page.

---
+++ src/Components/ServiceOverview/ServiceOverview.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {AiOutlineDoubleRight} from 'react-icons/ai';
+import './ServiceOverview.css';
+
+const ServiceOverview = ({ sImg, sName, sDesc, sLink }) => {
+    return (
+        <div className='serviceOverview'>
+            <div className="sImg">
+                <img src={sImg} alt={sName} />
+            </div>
+            <div className="sHead">
+                <h3>{sName}</h3>
+            </div>
+
+            <div className="sDesc">
+                <p>{sDesc}</p>
+            </div>
+
+            <Link to={`services/${sLink}`} className='link'><p className="seeMoreBtn">Learn More <AiOutlineDoubleRight /></p></Link>
+        </div>
+    )
+}
+
+export default ServiceOverview
