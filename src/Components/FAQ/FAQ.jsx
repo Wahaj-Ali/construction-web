c40b5452317db7518a6f5af64efe921e818c0116
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-10-26 02:01:06 +0500

    Redesign FAQ page.

--- src/Components/FAQ/FAQ.jsx
+++ src/Components/FAQ/FAQ.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {AiFillCaretDown, AiFillCaretUp} from 'react-icons/ai';
 import './FAQ.css';
 
 const FAQ = ({ data }) => {
+    const [visibleIndex, setVisibleIndex] = useState(null);
+
+    const toggleAnswer = (index) => {
+        setVisibleIndex(visibleIndex === index ? null : index);
+    };
+
     return (
-        <div className='FAQ' style={{ whiteSpace: 'pre-line' }}>
+        <div className='FAQ'>
             {data.map((item, index) => (
                 <div key={index} className='singleFAQ'>
-                    <div className="fQues">
+                    <div className="fQues" onClick={() => toggleAnswer(index)}>
                         <span id='qIcon'>Q.</span>
-                        <h3>{item.question}</h3>
+                        <h3>{item.question}{visibleIndex === index ? <AiFillCaretUp/> : <AiFillCaretDown />}</h3>
                     </div>
 
-                    <div className="fAns">
-                        <span id='aIcon'>A.</span>
-                        <p>{item.answer}</p>
-                    </div>
+                    {visibleIndex === index && (
+                        <div className={`fAns ${visibleIndex === index ? 'open' : ''}`}>
+                            <span id='aIcon'>A.</span>
+                            <p style={{ whiteSpace: 'pre-line' }}>{item.answer}</p>
+                        </div>
+                    )}
                 </div>
             ))}
         </div>
-    )
-}
+    );
+};
 
 export default FAQ
