d02d4743936ed584f215cf5ba3fd8052ad8cdcc1
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-10-27 00:26:42 +0500

    Add hero section with background image to the sservices page.

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -7,31 +7,37 @@ import interior from '../../Assets/Interior Painting.jpg';
 import floor from '../../Assets/Flooring Installation.jpg';
 import carpentry from '../../Assets/Carpentry and Trim Work.jpg';
 import ServiceOverview from '../../Components/ServiceOverview/ServiceOverview';
+import servicesBg from '../../Assets/services-bg.jpg';
 import './Services.css';
 
 const Services = () => {
     return (
-        <div className="servicesOverview sericePage">
-            <div className="homeHeading">
-                <h2>Our Services</h2>
+        <>
+            <div className='insuranceHero serviceHero'>
+                <img src={servicesBg} alt="Insurance Carriers" className='insuranceHeroImg shImg' />
+                <div className="insuranceHeroDesc">
+                    <h1 className="ihTtitle">Our Services</h1>
+                    {/* <h3>We are proud to be a trusted and prominent platform serving the construction and renovation community in New York. With a passion for bringing construction dreams to life, we've established ourselves as a reliable partner in the world of construction and renovation.</h3> */}
+                </div>
             </div>
+            <div className="servicesOverview">
+                <div className="hsSec">
+                    <ServiceOverview sImg={kitchen} currentPage="services" sName="Kitchen Remodeling" sDesc="Showcase your expertise in transforming kitchens, including cabinet installation, countertop upgrades, and layout redesigns." sLink="kitchen-remodeling" />
 
-            <div className="hsSec">
-                <ServiceOverview sImg={kitchen} currentPage="services" sName="Kitchen Remodeling" sDesc="Showcase your expertise in transforming kitchens, including cabinet installation, countertop upgrades, and layout redesigns." sLink="kitchen-remodeling" />
+                    <ServiceOverview sImg={bath} currentPage="services" sName="Bathroom Remodeling" sDesc="Highlight your ability to create beautiful and functional bathrooms through services like tile installation, fixture upgrades, and layout improvements." sLink="bathroom-remodeling" />
 
-                <ServiceOverview sImg={bath} currentPage="services" sName="Bathroom Remodeling" sDesc="Highlight your ability to create beautiful and functional bathrooms through services like tile installation, fixture upgrades, and layout improvements." sLink="bathroom-remodeling" />
+                    <ServiceOverview sImg={basement} currentPage="services" sName="Basement Finishing" sDesc="Promote your skills in turning unfinished basements into functional living spaces, such as recreation rooms, home offices, or additional bedrooms." sLink="basement-finishing" />
 
-                <ServiceOverview sImg={basement} currentPage="services" sName="Basement Finishing" sDesc="Promote your skills in turning unfinished basements into functional living spaces, such as recreation rooms, home offices, or additional bedrooms." sLink="basement-finishing" />
+                    <ServiceOverview sImg={room} currentPage="services" sName="Room Additions" sDesc="Emphasize your ability to expand homes by adding new rooms, whether it's a sunroom, a family room, or an extra bedroom." sLink="room-additions" />
 
-                <ServiceOverview sImg={room} currentPage="services" sName="Room Additions" sDesc="Emphasize your ability to expand homes by adding new rooms, whether it's a sunroom, a family room, or an extra bedroom." sLink="room-additions" />
+                    <ServiceOverview sImg={interior} currentPage="services" sName="Interior Painting" sDesc="Offer interior painting services to refresh and update the look of a home." sLink="interior-painting" />
 
-                <ServiceOverview sImg={interior} currentPage="services" sName="Interior Painting" sDesc="Offer interior painting services to refresh and update the look of a home." sLink="interior-painting" />
+                    <ServiceOverview sImg={floor} currentPage="services" sName="Flooring Installation" sDesc="Showcase your expertise in installing various types of flooring, such as hardwood, tile, laminate, or carpet." sLink="flooring-installation" />
 
-                <ServiceOverview sImg={floor} currentPage="services" sName="Flooring Installation" sDesc="Showcase your expertise in installing various types of flooring, such as hardwood, tile, laminate, or carpet." sLink="flooring-installation" />
-
-                <ServiceOverview sImg={carpentry} currentPage="services" sName="Carpentry and Trim Work" sDesc="Highlight your craftsmanship in custom carpentry, including trim, crown molding, and built-in cabinets." sLink="carpentary-and-trimwork" />
+                    <ServiceOverview sImg={carpentry} currentPage="services" sName="Carpentry and Trim Work" sDesc="Highlight your craftsmanship in custom carpentry, including trim, crown molding, and built-in cabinets." sLink="carpentary-and-trimwork" />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
