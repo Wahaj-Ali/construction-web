77df8c6d123b4ff550b2ac1bfdd13a500c4e8509
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-10-30 20:16:55 +0500

    Update ScrollToTop.jsx

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -1,8 +1,6 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router'
 
-import React from 'react'
-
 const ScrolltoTop = () => {
     const { pathname } = useLocation();
 
@@ -15,12 +13,3 @@ const ScrolltoTop = () => {
 
 export default ScrolltoTop
 
-// export const ScrollToTop = () => {
-//   const { pathname } = useLocation();
-
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, [pathname]);
-  
-//   return null;
-// }
