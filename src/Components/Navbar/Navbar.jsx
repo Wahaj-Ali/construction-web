cef2315db29628e3eb1c9badea4b5388225aea26
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-10-30 21:53:39 +0500

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
     <nav className="navbar">
       <div className="nav-container">
         <div className="logo">
-          <Link to="/"><img src={logo} alt="Fully Constructed" /></Link>
+          <Link to="/" onClick={closeMobileMenu}><img src={logo} alt="Fully Constructed" /></Link>
         </div>
         <div className="mobile-menu-button" onClick={toggleMobileMenu}>
           <span className="menu-icon">&#9776;</span>
