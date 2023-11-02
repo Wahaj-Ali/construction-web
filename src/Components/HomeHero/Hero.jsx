99a9c21afc634d10a845b028ce0ef17ced2dd179
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-11-02 18:06:39 +0500

    Chnage theme color

--- src/Components/HomeHero/Hero.jsx
+++ src/Components/HomeHero/Hero.jsx
@@ -8,7 +8,7 @@ export const Hero = () => {
     <div className='hero'>
       <img src={bg} alt="" className="heroImg" />
         <div className="heroDesc">
-          <h1 className="heroTitle">Fully Constructed: Gateway to Building Dreams</h1>
+          <h1 className="heroTitle">Fully Constructed Gateway to Building Dreams</h1>
           <h2 className="heroSubTitle">Empowering New York with Quality Construction and Renovation Solutions</h2>
           <Link to="/get-a-quote"><button className='quote-btn heroBtn'>Get Started</button></ Link>
         </div>
